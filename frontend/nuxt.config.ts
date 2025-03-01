c830d1e97a172e55657137095d696e65ac3c9337
HawkingHuang <math0915@gmail.com>
2025-03-01 14:43:34 +0800

    head settings

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -3,5 +3,16 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
-  css: ['@/assets/styles/main.scss']
+  css: ['@/assets/styles/main.scss'],
+  app: {
+    head: {
+     title: 'Traditional Chinese Checker',
+     meta: [
+      { name: 'description', content: 'Traditional Chinese Checker' }
+     ],
+     link: [
+      { rel: 'icon', type: 'image/png', sizes: '24x24', href: '/favicon-24.png'}
+     ]
+    }
+  }
 })
